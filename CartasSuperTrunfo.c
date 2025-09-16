4efb268ed0286449c81d67eadb5a85545d80c334
UniversitariaRafa <_rafaelanasc@outlook.com>
2025-09-16 22:45:28 +0000

    Criei o printf para o Tema 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
+  char Estado[20] = "A";
+  char CodigoCarta[20] = "A01";
+  char NomeCidade[20] = "Goiania";
+  int Populacao = 1456741;
+  float Area = 729.29; //km²
+  float PIB = 59.9; //bilhoes de reais
+  int NumerosPontosTuristicos = 60;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+ 
+  char estado[20] = "B";
+  char codigocarta[20] = "B02";
+  char nomecidade[20] = "Presidente Prudente";
+  int populacao = 234706;
+  float area = 560.63; //km²
+  float pib = 14.6; //milhoes de reais
+  int numerospontosturisticos = 20;
 
-  // Área para entrada de dados
+printf("Desafio Super Trunfo - Cidades\n");
+printf("Tema 1 - Cadrasto das Cartas\n");
 
-  // Área para exibição dos dados da cidade
+
+printf("Carta 1\n");
+printf("Estado: %s\n", Estado);
+printf("Código da Carta: %s\n", CodigoCarta);
+printf("Nome da Cidade: %s\n", NomeCidade);
+printf("População (milhões): %.1d\n", Populacao);
+printf("Área (km²): %.1f\n", Area);
+printf("PIB (bilhões): %.2f\n", PIB);
+printf("Numeros de Pontos Turísticos: %d\n", NumerosPontosTuristicos);
+printf("\n");
+
+printf("Carta 2\n");
+printf("Estado: %s\n", estado);
+printf("Código da Carta: %s\n", codigocarta);
+printf("Nome da Cidade: %s\n", nomecidade);
+printf("População (milhões): %.3d\n", populacao);
+printf("Área (km²): %.1f\n", area);
+printf("PIB (bilhões): %.1f\n", pib);
+printf("Numeros de Pontos Turísticos: %d\n", numerospontosturisticos);
 
 return 0;
-} 
+}
+
