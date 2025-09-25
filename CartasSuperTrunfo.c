0ab2ba6e056098430d5ae1d4a1dd8303d37148d2
UniversitariaRafa <_rafaelanasc@outlook.com>
2025-09-25 13:11:57 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@ int main(){
   char NomeCidade[20] = "Goiania";
   int Populacao = 1456741;
   float Area = 729.29; //km²
-  float PIB = 59.9; //bilhoes de reais
+  float PIB = 59.90; //bilhoes de reais
   int NumerosPontosTuristicos = 60;
 
  
@@ -15,11 +15,12 @@ int main(){
   char nomecidade[20] = "Presidente Prudente";
   int populacao = 234706;
   float area = 560.63; //km²
-  float pib = 14.6; //milhoes de reais
+  float pib = 14.60; //milhoes de reais
   int numerospontosturisticos = 20;
 
 printf("Desafio Super Trunfo - Cidades\n");
 printf("Tema 1 - Cadrasto das Cartas\n");
+printf("\n");
 
 
 printf("Carta 1\n");
@@ -27,7 +28,7 @@ printf("Estado: %s\n", Estado);
 printf("Código da Carta: %s\n", CodigoCarta);
 printf("Nome da Cidade: %s\n", NomeCidade);
 printf("População (milhões): %.1d\n", Populacao);
-printf("Área (km²): %.1f\n", Area);
+printf("Área (km²): %.2f\n", Area);
 printf("PIB (bilhões): %.2f\n", PIB);
 printf("Numeros de Pontos Turísticos: %d\n", NumerosPontosTuristicos);
 printf("\n");
@@ -37,8 +38,8 @@ printf("Estado: %s\n", estado);
 printf("Código da Carta: %s\n", codigocarta);
 printf("Nome da Cidade: %s\n", nomecidade);
 printf("População (milhões): %.3d\n", populacao);
-printf("Área (km²): %.1f\n", area);
-printf("PIB (bilhões): %.1f\n", pib);
+printf("Área (km²): %.2f\n", area);
+printf("PIB (bilhões): %.2f\n", pib);
 printf("Numeros de Pontos Turísticos: %d\n", numerospontosturisticos);
 
 return 0;
